8957f32a46781d83b65420ed4998ba6243956589
Norberthue <kokanorbert9@gmail.com>
2025-03-27 00:58:54 +0100

    add lot of stuff
    added levels, points to level up stats, battle over log , continue after battle to pick new enemy.

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let player_stats = {
     luck:10,
     strength:8,
     health: 10, 
-    damage: 10,
+    damage: 330,
     armor: 25,
+    level: 1,
+    levelProgress: 0,
+    points: 0,
 }
 
 let enemy_stats = {
@@ -18,6 +21,7 @@ let enemy_stats = {
     health: 10,
     damage: 10,
     armor: 50,
+    level: 1,
 }
 
 //player stats decleration
@@ -64,6 +68,8 @@ e_dmg.innerHTML= enemy_stats.damage
 const e_armor = document.getElementById('e-armor')
 e_armor.innerHTML= enemy_stats.armor
 
+//player points
+const p_points = document.getElementById('p-points')
 
 //player health
 player_stats.health = player_stats.health * player_stats.vitality 
@@ -72,9 +78,21 @@ const player_health_element = document.getElementById('p-health')
 player_health_element.setAttribute('max', player_stats.health)
 player_health_element.value = player_stats.health
 //player health bar text
-let player_health_text_element = document.getElementById('p-health-text')
-player_health_text_element.innerHTML = player_stats.health
+const player_health_text_element = document.getElementById('p-health-text')
+player_health_text_element.innerHTML = player_stats.health + '/' + player_health_element.getAttribute('max')
 
+//player level 
+let max_level_progress = 10
+let exp_gain = 11
+const player_level_progress_element = document.getElementById('p-level')
+player_level_progress_element.setAttribute('max', max_level_progress)
+player_level_progress_element.value = player_stats.levelProgress
+
+const player_level_text_element = document.getElementById('player-level-text')
+player_level_text_element.innerHTML = player_stats.level
+
+const player_level_text_progress_element = document.getElementById('player-level-progress-text')
+player_level_text_progress_element.innerHTML = player_stats.levelProgress + '/' + player_level_progress_element.getAttribute('max')
 
 //enemy health
 enemy_stats.health = enemy_stats.health* enemy_stats.vitality
@@ -84,43 +102,177 @@ enemmy_health_element.setAttribute('max', enemy_stats.health)
 enemmy_health_element.value = enemy_stats.health
 //enemy health bar text
 const enemmy_health_text_element = document.getElementById('e-health-text')
-enemmy_health_text_element.innerHTML = enemy_stats.health
+enemmy_health_text_element.innerHTML = enemy_stats.health + '/' + enemmy_health_element.getAttribute('max')
 
 
-//battle log 
+//battle variables
 const battle_log_text_element = document.getElementById('battle-log-text')
-
-
+const pick_new_enemy_element = document.getElementById('pick-enemy-container')
+const battle_log_title_element = document.getElementsByClassName('battle-text')
+console.log(battle_log_title_element)
 let player_attacks = true
 
-const battle = () => {
-    setInterval(() => {
-        if (player_attacks ) {
-            let damage = (player_stats.damage - ((player_stats.damage / 100) * enemy_stats.armor))
-            enemy_stats.health -= damage
-            enemmy_health_element.value = enemy_stats.health
-            enemmy_health_text_element.innerHTML = enemy_stats.health
-            player_attacks = false
+
+const update_player_level_stats_ui = () => {
+    player_level_progress_element.setAttribute('max', max_level_progress)
+    player_level_progress_element.value = player_stats.levelProgress
+    player_level_text_element.innerHTML = player_stats.level
+    player_level_text_progress_element.innerHTML = player_stats.levelProgress + '/' + player_level_progress_element.getAttribute('max')
+    p_points.innerHTML = player_stats.points
+}
+
+const update_player_stats_ui = () => {
+    p_vit.innerHTML= player_stats.vitality
+    p_dex.innerHTML= player_stats.dexterity
+    p_int.innerHTML= player_stats.intelligence
+    p_lck.innerHTML= player_stats.luck
+    p_str.innerHTML= player_stats.strength
+    p_points.innerHTML = player_stats.points
+    //update health 
+    player_stats.health = 10 * player_stats.vitality 
+    player_health_element.setAttribute('max', player_stats.health)
+    player_health_text_element.innerHTML = player_stats.health + '/' + player_health_element.getAttribute('max')
+    player_health_element.value = player_stats.health
+}
+
+const pickNewEnemy = () => {
+    //clean up the board
+    element = battle_log_text_element.getElementsByTagName('div')
+    element_btn = battle_log_text_element.getElementsByTagName('button')
+    element_btn[0].parentNode.removeChild(element_btn[0])
+    for (i = element.length -1 ; i >= 0 ; i--) {
+        element[i].parentNode.removeChild(element[i])
+    }
+    //reset player health
+    update_player_stats_ui()
+    //hide battle log
+    battle_log_text_element.style.visibility = 'hidden'
+    battle_log_text_element.style.position = 'absolute'
+    //show enemies container
+    pick_new_enemy_element.style.visibility = 'visible'
+    pick_new_enemy_element.style.position = 'initial'
+    //change the title
+    battle_log_title_element[0].innerHTML= 'Pick your next enemy'
+
+}
+
+
+
+const battle_over = () => {
+    if (player_stats.health > 0) {
+        //battle log
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = 'Player has won !\n Reward: ' +  exp_gain + 'exp'
+        battle_log_text_element.appendChild(newDiv)
+        
+        //update player exeperience and level
+        player_stats.levelProgress += exp_gain
+        if ( player_stats.levelProgress >= max_level_progress) {
+            player_stats.levelProgress = player_stats.levelProgress - max_level_progress
+            max_level_progress += 5
+            player_stats.level += 1   
+            player_stats.points += 10
             const newDiv = document.createElement('div')
-            newDiv.innerHTML = 'Player does ' + damage + ' damage to Enemy'
+            newDiv.innerHTML = 'Players level has increase to level ' + player_stats.level + '.'
             battle_log_text_element.appendChild(newDiv)
-        } else {
-            let damage = (player_stats.damage - ((player_stats.damage / 100) * enemy_stats.armor))
-            player_stats.health -= damage
-            player_health_text_element.innerHTML = player_stats.health
-            player_health_element.value = player_stats.health
-            player_attacks = true 
-            const newDiv = document.createElement('div')
-            newDiv.innerHTML = 'Enemy does ' + damage + ' damage to Player'
-            battle_log_text_element.appendChild(newDiv)
+            update_player_level_stats_ui()
         }
+
+        //continue btt to next enemy
+        const newBtn = document.createElement('button')
+        newBtn.innerHTML = 'Countinue'
+        newBtn.style.marginTop = '5px'
+        newBtn.addEventListener('click', pickNewEnemy)
+        battle_log_text_element.appendChild(newBtn)
+    } else {
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = 'Enemy has won !' 
+        battle_log_text_element.appendChild(newDiv)
+    }
+}
+
+const battle = () => {
+    const battle_interval = setInterval(() => {
+        if (player_stats.health > 0 && enemy_stats.health > 0) {
+            if (player_attacks ) {
+                //math for fight
+                let armor_block = Math.floor(((player_stats.damage / 100) * enemy_stats.armor))
+                let damage = Math.floor((player_stats.damage - armor_block))
+                enemy_stats.health -= damage
+                enemmy_health_element.value = enemy_stats.health
+                enemmy_health_text_element.innerHTML = enemy_stats.health + '/' + enemmy_health_element.getAttribute('max')
+                player_attacks = false
+                //battle log
+                const newDiv = document.createElement('div')
+                newDiv.innerHTML = 'Player does ' + damage + ' damage to Enemy. Enemy Blocks ' + armor_block + ' damage with his armor.'
+                battle_log_text_element.appendChild(newDiv)
+            } else {
+                //math for battle
+                const armor_block =  Math.floor((enemy_stats.damage / 100) * enemy_stats.armor)
+                const damage = Math.floor((enemy_stats.damage - armor_block))
+                player_stats.health -= damage
+                player_health_text_element.innerHTML = player_stats.health + '/' + player_health_element.getAttribute('max')
+                player_health_element.value = player_stats.health
+                player_attacks = true 
+                //battle log
+                const newDiv = document.createElement('div')
+                newDiv.innerHTML = 'Enemy does ' + damage + ' damage to Player. Player Blocks ' + armor_block + ' damage with his armor.'
+                battle_log_text_element.appendChild(newDiv)
+            }
+        } else {
+            clearInterval(battle_interval)
+            //clean battle log
+            var element = battle_log_text_element.getElementsByTagName('div');
+            for (i = element.length - 1; i >= 0; i--) {
+                element[i].parentNode.removeChild(element[i]);
+            }
+            battle_over()
+        }
+       
     },1000)
 }
 
 //get start battle button 
-const start_battle =  document.getElementById('start-battle-btt-js')
-start_battle.addEventListener('click', battle)
-   
+const start_battle = document.getElementById('start-battle-btt-js')
+start_battle.addEventListener('click', () => {
+    battle()
+    start_battle.style.visibility= 'hidden'
+})
+
+
+//get all button for upgrade
+const upgrade_btt = document.querySelectorAll('[id^=plus-stats]')
+upgrade_btt.forEach((btn) => btn.addEventListener('click', () => {
+    if (btn.getAttribute('data-id') === 'vit' && player_stats.points > 0) {
+        player_stats.vitality += 1    
+        player_stats.points -=1
+        update_player_stats_ui()
+    }
+
+    if (btn.getAttribute('data-id') === 'dex' && player_stats.points > 0) {
+        player_stats.dexterity+= 1    
+        player_stats.points -=1
+        update_player_stats_ui()
+    }
+
+    if (btn.getAttribute('data-id') === 'int' && player_stats.points > 0) {
+        player_stats.intelligence+= 1    
+        player_stats.points -=1
+        update_player_stats_ui()
+    }
+
+    if (btn.getAttribute('data-id') === 'lck' && player_stats.points > 0) {
+        player_stats.luck+= 1    
+        player_stats.points -=1
+        update_player_stats_ui()
+    }
+
+    if (btn.getAttribute('data-id') === 'str' && player_stats.points > 0) {
+        player_stats.strength += 1    
+        player_stats.points -=1
+        update_player_stats_ui()
+    }
+}))
 
 
 
